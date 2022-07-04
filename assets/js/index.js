159f2cb113925492091b59dad077ba3bf4915c31
Mario José Pineda <mjp0997@gmail.com>
2022-07-04 18:32:37 -0400

    Funcionalidad para abrir los arcos con el sidebar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,19 @@
 // Arcos de la trama
 
+const links = document.querySelectorAll('.aux-link');
+
+
+// Listeners
+
 document.addEventListener('DOMContentLoaded', function() {
    var elems = document.querySelectorAll('.collapsible');
    M.Collapsible.init(elems);
 });
+
+links.forEach(link => {
+   link.addEventListener('click', (e) => {
+      const element = document.querySelector('#' + e.target.dataset.value);
+
+      element.children[0].click();
+   });
+})
